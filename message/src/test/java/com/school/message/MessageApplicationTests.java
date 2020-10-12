a3b667b374de906a155dbc6d127776e4c398cb47
feiyu <164991810@qq.com>
2020-10-12 11:53:31 +0800

    t

--- message/src/test/java/com/school/message/MessageApplicationTests.java
+++ message/src/test/java/com/school/message/MessageApplicationTests.java
@@ -1,28 +1,35 @@
 package com.school.message;
 
+import com.school.message.message.ISendMessage;
+import com.school.message.message.MessageHandler;
+import com.school.message.message.message.EMailMessage;
 import com.school.message.message.weixin.SendWeixin;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.mail.MessagingException;
 import java.net.URISyntaxException;
 
 @SpringBootTest
 class MessageApplicationTests {
 
 
-	private SendWeixin sendWeixin;
+//	private SendWeixin sendWeixin;
 
+	private MessageHandler messageHandler;
+	ISendMessage sendEmail;
 	@Autowired
-	public MessageApplicationTests(SendWeixin sendWeixin) {
-		this.sendWeixin = sendWeixin;
+	public MessageApplicationTests(MessageHandler messageHandler,ISendMessage sendEmail) {
+		this.messageHandler = messageHandler;
+		this.sendEmail = sendEmail;
 	}
 
-	@Test
-	public void test() throws URISyntaxException {
-
-		System.out.println(sendWeixin.getOpenId());
-	}
+//	@Test
+//	public void test() throws URISyntaxException {
+//
+//		System.out.println(sendWeixin.getOpenId());
+//	}
 	//	private MessageHandler messageHandler;
 //	private ISendMessage sendEmail;
 //
@@ -35,23 +42,23 @@ class MessageApplicationTests {
 //		this.getEmail = getEmail;
 //	}
 //
-//	@Test
-//	void contextLoads() throws MessagingException {
-//		EMailMessage message = new EMailMessage();
-//		message.setFrom("164991810@qq.com");
-//		message.setTo(new String[]{"wan.guangming@heitao.com"});
-//		message.setContent("测试");
-//		message.setSubject("lalala");
-//
-//		message.setHost("smtp.qq.com");
-//		message.setPassword("xmxgiekmhtrubjjb");
-//		message.setProtocol("smtp");
-//		message.setUsername("164991810@qq.com");
-//		messageHandler.init(sendEmail);
-//		messageHandler.send(message);
-//
-////		getEmail.handler();
-//	}
+	@Test
+	void contextLoads() throws MessagingException {
+		EMailMessage message = new EMailMessage();
+		message.setFrom("164991810@qq.com");
+		message.setTo(new String[]{"gjy950227@163.com"});
+		message.setContent("测试");
+		message.setSubject("lalala");
+
+		message.setHost("smtp.qq.com");
+		message.setPassword("xmxgiekmhtrubjjb");
+		message.setProtocol("smtp");
+		message.setUsername("164991810@qq.com");
+		messageHandler.init(sendEmail);
+		messageHandler.send(message);
+
+//		getEmail.handler();
+	}
 //
 //	public void test() throws MessagingException {
 //
